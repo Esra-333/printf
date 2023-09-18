1cecf9199e1825e08ed33ffb0d2922bc3f5e5fe9
Esra-333 <esra.omer444@gmail.com>
2023-09-18 06:13:35 -0700

    a

--- main.h
+++ main.h
@@ -45,13 +45,13 @@ int flags, int width, int precision, int size);
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
-int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_string(va_list types, char buffer[],
-int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_percent(va_list types, char buffer[],
-int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
 int print_int(va_list types, char buffer[],
