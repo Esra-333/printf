f0901c83d946e297770588e733b45f9cbba09188
Esra-333 <esra.omer444@gmail.com>
2023-09-17 23:04:42 -0700

    a

--- functions1.c
+++ functions1.c
@@ -115,7 +115,8 @@ int flags, int width, int precision, int size)
 int print_hexadecimal(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
-	return (print_hexa(types, "0123456789abcdef", buffer, flags, 'x', width, precision, size));
+	return (print_hexa(types, "0123456789abcdef",
+buffer, flags, 'x', width, precision, size));
 }
 /* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL */
 /**
@@ -131,7 +132,8 @@ int flags, int width, int precision, int size)
 int print_hexa_upper(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
-	return (print_hexa(types, "0123456789ABCDEF", buffer, flags, 'X', width, precision, size));
+	return (print_hexa(types, "0123456789ABCDEF",
+buffer, flags, 'X', width, precision, size));
 }
 /* PRINT HEXX NUM IN LOWER OR UPPER */
 /**
@@ -147,8 +149,7 @@ int flags, int width, int precision, int size)
  * Return: Number of chars printed
  */
 int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width,
-int precision, int size)
+char buffer[], int flags, char flag_ch, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
