cfb4df3dc5435e0a4fd460b4976fda3018c6e42c
malaz <malaz.hatim@gmail.com>
2023-09-19 15:01:14 -0700

    m

--- buffer.c
+++ buffer.c
@@ -2,14 +2,17 @@
 
 /**
  * print_buffer - Prints the contents of the buffer
- * @buffer: Array 
+ * @buffer: Array
  * @buff_ind: Index at which to add to buffer
  */
 
-void print_buffer(char buffer[], int *buff);
+void print_buffer(char buffer[], int *buff)
+
 {
 	if (*buff > 0)
+	{
 		write(1, &buffer[0], *buff);
+	}
 
 	*buff = 0;
 }
