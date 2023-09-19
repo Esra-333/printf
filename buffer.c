1768fb6e0bac0702aa6f12b54e72c0852271f5fb
malaz <malaz.hatim@gmail.com>
2023-09-19 14:46:03 -0700

    m

--- buffer.c
+++ buffer.c
@@ -6,10 +6,10 @@
  * @buff_ind: Index at which to add to buffer
  */
 
-void print_buffer(char buffer[], int *buffer);
+void print_buffer(char buffer[], int *buff);
 {
-	if (*buffer > 0)
-		write(1, &buffer[0], *buffer);
+	if (*buff > 0)
+		write(1, &buffer[0], *buff);
 
-	*buffer = 0;
+	*buff = 0;
 }
