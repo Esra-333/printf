8bf92374e53565f51084be53ee3faebcf32ef3ba
malaz <malaz.hatim@gmail.com>
2023-09-19 14:47:17 -0700

    m

--- main.h
+++ main.h
@@ -25,7 +25,7 @@ typedef struct specifiers
 int _printf(const char *format, ...);
 int get_function(char s, va_list args);
 int _putchar(char c);
-void print_buffer(char buffer[], int *buffer);
+void print_buffer(char buffer[], int *buff);
 
 /*Conversion specifiers*/
 
